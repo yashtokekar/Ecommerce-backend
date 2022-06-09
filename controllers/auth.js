9ab591bcf1c0c47a92f67f57ee05618fb26dbc39
Yash Tokekar <yash.tokekar@gmail.com>
2022-06-09 20:00:30 +0530

    Create or Update User

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,34 @@
-exports.createOrUpdateUser = (req,res) => {
-    res.json({
-        data: "create or update user"
+const User = require("../models/user");
+
+
+exports.createOrUpdateUser = async (req,res) => {
+    const { name, picture, email } = req.user;
+
+    const user = await User.findOneAndUpdate(
+        {email}, 
+        {name: email.split('@')[0], picture}, 
+        {new: true}
+    );
+
+    if(user) {
+        console.log("User Updated", user);
+        res.json(user);
+        
+    } else {
+        const newUser = await new User({
+            email,
+            name: email.split('@')[0],
+            picture,
+        }).save();
+        console.log("User Created", newUser);
+        res.json(newUser);
+    }
+};
+
+exports.currentUser = async (req,res) => {
+    User.findOne({email: req.user.email}).exec((err,user) => {
+        if(err) throw new Error(err);
+        res.json(user);
     });
 };
 
