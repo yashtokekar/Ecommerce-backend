637c21e9a3bcede0eb95241633c01c45a80cf103
Yash Tokekar <yash.tokekar@gmail.com>
2022-06-10 20:46:20 +0530

    Admin Route and Middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin  = require("../firebase");
+const User = require("../models/user")
 
 exports.authCheck = async (req,res,next) =>{
     
@@ -15,4 +16,20 @@ exports.authCheck = async (req,res,next) =>{
             err: "Invalid or expired token"
         });
     }
+};
+
+exports.adminCheck = async (req,res,next) => {
+    const {email} = req.user;
+
+    const adminUser = await User.findOne({ email }).exec();
+
+    if(adminUser.role !== "admin") {
+        res.status(403).json({
+            err: "Admin resource. Access denied.",
+        });
+    } else {
+        next();
+    }
+
+
 };
