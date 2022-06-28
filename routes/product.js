04b939bcca0a22bc65e835d420a0d245c9cf1953
Yash Tokekar <yash.tokekar@gmail.com>
2022-06-28 15:03:58 +0530

    Get Related products endpoint

--- routes/product.js
+++ routes/product.js
@@ -15,6 +15,7 @@ const {
   list,
   productsCount,
   productStar,
+  listRelated,
 } = require('../controllers/product');
 
 //routes
@@ -25,6 +26,9 @@ router.delete('/product/:slug', authCheck, adminCheck, remove);
 router.get('/product/:slug', read);
 router.put('/product/:slug', authCheck, adminCheck, update);
 router.post('/products', list);
+//rating
 router.put('/product/star/:productId', authCheck, productStar);
+//related
+router.get('/product/related/:productId', listRelated);
 
 module.exports = router;
