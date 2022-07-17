1b56633325acd168e5f6987bb7667fd112465d8f
Yash Tokekar <yash.tokekar@gmail.com>
2022-07-17 16:18:57 +0530

    Checkout Complete

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/user", (req,res) => {
-    res.json({
-        data: "user API endpoint"
-    });
-});
+//middleware
+const { authCheck } = require('../middlewares/auth');
+
+//controller
+const {
+  userCart,
+  getUserCart,
+  emptyCart,
+  saveAddress,
+} = require('../controllers/user');
+
+router.post('/user/cart', authCheck, userCart); // save cart
+router.get('/user/cart', authCheck, getUserCart); // get cart
+router.delete('/user/cart', authCheck, emptyCart); // empty cart
+router.post('/user/address', authCheck, saveAddress); // empty cart
 
 module.exports = router;
