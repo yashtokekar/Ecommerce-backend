2951640f7bbbb6a76a8968fd0273116de1b38a7e
Yash Tokekar <yash.tokekar@gmail.com>
2022-07-21 13:46:17 +0530

    Apply coupon to user cart

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const Coupon = require('../models/coupon');
 
 exports.userCart = async (req, res) => {
   const { cart } = req.body;
@@ -74,4 +75,35 @@ exports.saveAddress = async (req, res) => {
   ).exec();
 
   res.json({ ok: true });
+};
+
+exports.applyCouponToUserCart = async (req, res) => {
+  const { coupon } = req.body;
+  const validCoupon = await Coupon.findOne({ name: coupon }).exec();
+  if (validCoupon === null) {
+    return res.json({
+      err: 'Invalid Coupon',
+    });
+  }
+  console.log('Valid Coupon', validCoupon);
+
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  let { products, cartTotal } = await Cart.findOne({ orderdBy: user._id })
+    .populate('products.product', '_id title price')
+    .exec();
+
+  // total after dicount
+  let totalAfterDiscount = (
+    cartTotal -
+    (cartTotal * validCoupon.discount) / 100
+  ).toFixed(2); //upto 2 decimal place
+
+  await Cart.findOneAndUpdate(
+    { orderdBy: user._id },
+    { totalAfterDiscount },
+    { new: true }
+  );
+
+  res.json(totalAfterDiscount);
 };
